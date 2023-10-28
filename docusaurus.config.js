21ed0b2a9b29ba3fa45f3be9821106478c53386b
Elbert  Ribeiro <elbertprofissional@gmail.com>
2023-10-27 21:48:24 -0300

    atualizando Copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Elbert Ribeiro Site, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
